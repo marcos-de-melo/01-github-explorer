1a0086cb66573dcbf99f414bcc1b89da258776c6
Marcos de Melo <marcdmelo@gmail.com>
2021-08-29 18:44:34 -0300

    Modulo Fundamentos Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Couter } from './components/Counter';
 import { Repositirylist } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App(){
   
     return (
-        <>
             <Repositirylist/>
-            <Couter/>
-        </>
     );
 }
